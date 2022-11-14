f96891022c7f027c3f68dd2dfa1cf13608f5206f
Joel Munoz <j.munoz1@yahoo.com>
2022-11-13 20:47:42 -0800

    small updates on index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import  reactDom  from "react-dom";
 
 import App from "./pages/Home";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-git
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.render(
+    <App />,
+    rootElement
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import "../style/styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello</h1>
+      <h2>ready to build something Brandon?</h2>
     </div>
   );
 }
